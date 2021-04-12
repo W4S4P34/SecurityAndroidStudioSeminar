6894af73b04ce27e59168409a0fd7eede5183418
Where is Neo? <alec.bui.vhp@gmail.com>
2021-04-13 04:43:03 +0700

    (!) Main Activity edit

--- app/src/main/java/com/example/initialdemo_seminar/MainActivity.java
+++ app/src/main/java/com/example/initialdemo_seminar/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.initialdemo_seminar;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this, AndroidKeyStoreDemo.class);
+        startActivity(intent);
     }
 }
