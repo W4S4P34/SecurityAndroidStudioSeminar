565b16589c429e1814e2a44f33b81f57dd71cab4
La Thành Thái <lathanhthai1905@gmail.com>
2021-04-13 21:48:35 +0700

    (+) Add layout for demo from @alecbuivhp

--- app/src/main/java/com/example/initialdemo_seminar/EncryptedFileDemo.java
+++ app/src/main/java/com/example/initialdemo_seminar/EncryptedFileDemo.java
@@ -3,13 +3,19 @@ package com.example.initialdemo_seminar;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.security.crypto.EncryptedFile;
 import androidx.security.crypto.MasterKey;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -20,6 +26,10 @@ import java.util.Arrays;
 public class EncryptedFileDemo extends AppCompatActivity {
     Context context;
     MasterKey mainKey;
+    EditText textFileName;
+    EditText textInput;
+    TextView textDecryptData;
+    TextView textEncryptData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,17 +44,70 @@ public class EncryptedFileDemo extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        Log.d("MY_DEMO", String.valueOf(context.getFilesDir()));
+        textFileName = findViewById(R.id.file_name);
+        textInput = findViewById(R.id.input_string);
+        textDecryptData = findViewById(R.id.textDecryptData);
+        textEncryptData = findViewById(R.id.textEncryptData);
 
-        writeFile("Hello world");
-        readFile();
+        findViewById(R.id.buttonSaveFile).setOnClickListener(v -> {
+            String filename = textFileName.getText().toString();
+            String data = textInput.getText().toString();
+
+            if (!filename.isEmpty() && !data.isEmpty()) {
+                writeFile(filename, data);
+            }
+
+            textEncryptData.setText("");
+            textDecryptData.setText("");
+
+            Toast toast = new Toast(getApplicationContext());
+            toast.setText("File is saved");
+            toast.show();
+        });
+
+        findViewById(R.id.buttonReadFileNormal).setOnClickListener(v -> {
+            String filename = textFileName.getText().toString();
+            if (!filename.isEmpty()) {
+                readFileNormal(filename);
+            }
+        });
+
+
+        findViewById(R.id.buttonReadFileEncrypted).setOnClickListener(v -> {
+            String filename = textFileName.getText().toString();
+            if (!filename.isEmpty()) {
+                readFile(filename);
+            }
+        });
     }
 
-    private void readFile() {
+    private void readFileNormal(String filename) {
+        File file = new File(getFilesDir(), filename);
+
+        StringBuilder text = new StringBuilder();
+
         try {
-            String fileToRead = "my_sensitive_data.txt";
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                text.append(line);
+                text.append('\n');
+            }
+            br.close();
+        } catch (IOException e) {
+            Toast toast = new Toast(getApplicationContext());
+            toast.setText(e.getMessage());
+            toast.show();
+        }
+
+        textEncryptData.setText(text);
+    }
+
+    private void readFile(String filename) {
+        try {
             EncryptedFile encryptedFile = new EncryptedFile.Builder(context,
-                    new File(getFilesDir(), fileToRead),
+                    new File(getFilesDir(), filename),
                     mainKey,
                     EncryptedFile.FileEncryptionScheme.AES256_GCM_HKDF_4KB
             ).build();
@@ -58,20 +121,23 @@ public class EncryptedFileDemo extends AppCompatActivity {
             }
 
             byte[] plaintext = byteArrayOutputStream.toByteArray();
-            Log.d("MY_DEMO", new String(plaintext));
+
+            String result = new String(plaintext);
+            textDecryptData.setText(result);
 
             byteArrayOutputStream.close();
             inputStream.close();
         } catch (GeneralSecurityException | IOException e) {
-            e.printStackTrace();
+            Toast toast = new Toast(getApplicationContext());
+            toast.setText(e.getMessage());
+            toast.show();
         }
     }
 
-    private void writeFile(String data) {
+    private void writeFile(String filename, String data) {
         try {
-            String fileToWrite = "my_sensitive_data.txt";
             EncryptedFile encryptedFile = new EncryptedFile.Builder(context,
-                    new File(getFilesDir(), fileToWrite),
+                    new File(getFilesDir(), filename),
                     mainKey,
                     EncryptedFile.FileEncryptionScheme.AES256_GCM_HKDF_4KB
             ).build();
@@ -82,7 +148,9 @@ public class EncryptedFileDemo extends AppCompatActivity {
             outputStream.flush();
             outputStream.close();
         } catch (GeneralSecurityException | IOException e) {
-            e.printStackTrace();
+            Toast toast = new Toast(getApplicationContext());
+            toast.setText(e.getMessage());
+            toast.show();
         }
     }
 }
--- app/src/main/java/com/example/initialdemo_seminar/EncryptedSharedPreferencesDemo.java
+++ app/src/main/java/com/example/initialdemo_seminar/EncryptedSharedPreferencesDemo.java
@@ -1,8 +1,13 @@
 package com.example.initialdemo_seminar;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.security.crypto.EncryptedSharedPreferences;
@@ -10,37 +15,97 @@ import androidx.security.crypto.MasterKey;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.Map;
 
 public class EncryptedSharedPreferencesDemo extends AppCompatActivity {
+    private final String FILE_NAME_ENCRYPTED = "FILE_ENCRYPTED";
+    private final String FILE_NAME_NORMAL = "FILE_NORMAL";
+
+    MasterKey mainKey;
+
+    SharedPreferences normalPref;
+    SharedPreferences.Editor normalEditor;
+    SharedPreferences encryptedPref;
+    SharedPreferences.Editor encryptedEditor;
+
+    EditText textKey;
+    EditText textValue;
+    TextView textPairs;
+    TextView textDecryptPairs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_encrypted_shared_preferences_demo);
 
+        textKey = findViewById(R.id.textKey);
+        textValue = findViewById(R.id.textValue);
+        textPairs = findViewById(R.id.textPairs);
+        textDecryptPairs = findViewById(R.id.textDecryptData);
+
         try {
-            MasterKey mainKey = new MasterKey.Builder(getApplicationContext())
+            mainKey = new MasterKey.Builder(getApplicationContext())
                     .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
                     .build();
 
-            SharedPreferences pref = EncryptedSharedPreferences.create(getApplicationContext(),
-                    "FILE",
+            normalPref = getSharedPreferences(FILE_NAME_NORMAL, Context.MODE_PRIVATE);
+            normalEditor = normalPref.edit();
+
+            encryptedPref = EncryptedSharedPreferences.create(getApplicationContext(),
+                    FILE_NAME_ENCRYPTED,
                     mainKey,
                     EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
                     EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM);
-
-            SharedPreferences.Editor editor = pref.edit();
-
-            editor.putString("name", "A name here");
-            editor.putBoolean("isDev", true);
-            editor.apply();
-
-            String string = pref.getString("name", "No name here");
-            Log.d("MY_DEMO", string);
-
+            encryptedEditor = encryptedPref.edit();
         } catch (GeneralSecurityException | IOException e) {
             e.printStackTrace();
         }
 
+        findViewById(R.id.buttonAdd).setOnClickListener(v -> {
+            String key = textKey.getText().toString();
+            String value = textValue.getText().toString();
+
+            normalEditor.putString(key, value);
+            normalEditor.apply();
+
+            Map<String, ?> mapAll = getNormal();
+            String temp = "";
+
+            for (Map.Entry<String, ?> entry : mapAll.entrySet())
+                temp += entry.getKey() + ": " + entry.getValue() + "\n";
+
+            textPairs.setText(temp);
+        });
+
+        findViewById(R.id.buttonAddStorage).setOnClickListener(v -> {
+            Map<String, ?> mapAll = getNormal();
+
+            for (Map.Entry<String, ?> entry : mapAll.entrySet())
+                encryptedEditor.putString(entry.getKey(), (String) entry.getValue());
+
+            encryptedEditor.apply();
+
+            Toast toast = new Toast(getApplicationContext());
+            toast.setText("Data added");
+            toast.show();
+        });
+
+        findViewById(R.id.buttonGetReadable).setOnClickListener(v -> {
+            Map<String, ?> mapAllEncrypted = getEncrypted();
+            String temp = "";
+
+            for (Map.Entry<String, ?> entry : mapAllEncrypted.entrySet())
+                temp += entry.getKey() + ": " + entry.getValue() + "\n";
+
+            textDecryptPairs.setText(temp);
+        });
+    }
+
+    private Map<String, ?> getNormal() {
+        return normalPref.getAll();
+    }
+
+    private Map<String, ?> getEncrypted() {
+        return encryptedPref.getAll();
     }
 }
